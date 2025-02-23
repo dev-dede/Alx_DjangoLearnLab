53bd4c5f16a46691fb0e35dcb00f09eb0c8f4964
dev-dede <tsewayog@gmail.com>
2025-02-23 04:31:35 +0000

    Use variable names to store author and library name in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,13 +8,15 @@ django.setup()
 from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="Dede")
+author_name = "Dede"
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all()
 print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
 
 
 # List all books in a specific library
-library = Library.objects.get(name="Moi_lib")
+library_name = "Moi_lib"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 print(f"Books in {library.name} library: {[book.title for book in books_in_library]}")
 
