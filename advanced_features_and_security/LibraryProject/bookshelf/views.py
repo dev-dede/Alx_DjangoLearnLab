5e511e8663e9206655e2f8e3b8ee7c28a5d18fb4
dev-dede <tsewayog@gmail.com>
2025-03-02 23:54:39 +0000

    Test

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,26 +5,26 @@ from django.db import models
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseForbidden
-from .models import Article
+from .models import Book
 
-# View for editing an article
-@permission_required('myapp.can_edit', raise_exception=True)
-def edit_article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
+# View for editing an book
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(book, id=book_id)
     
     if request.method == "POST":
-        article.title = request.POST.get("title")
-        article.content = request.POST.get("content")
-        article.save()
-        return redirect("article_detail", article_id=article.id)
+        book.title = request.POST.get("title")
+        book.content = request.POST.get("content")
+        book.save()
+        return redirect("book_detail", book_id=book.id)
     
-    return render(request, "edit_article.html", {"article": article})
+    return render(request, "edit_book.html", {"book": book})
 
-# View for deleting an article
-@permission_required('myapp.can_delete', raise_exception=True)
-def delete_article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
-    article.delete()
-    return redirect("article_list")
+# View for deleting an book
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(book, id=book_id)
+    book.delete()
+    return redirect("book_list")
 
     
