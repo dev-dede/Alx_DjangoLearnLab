d34f91dfdc3efa430f123a3f9736de7cf5e1e42c
dev-dede <tsewayog@gmail.com>
2025-03-09 21:01:47 +0000

    Remove overridden permission class; already set globally

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,13 +8,5 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    """
-    This view requires authentication.
-    Users must provide a valid token in the request header.
-    Example:
-        Authorization: Token <your_token_here>
-    """
-
-    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
