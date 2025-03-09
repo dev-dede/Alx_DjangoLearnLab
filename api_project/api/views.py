e81051fb49c3773bc1601415d2a8e879af469e3a
dev-dede <tsewayog@gmail.com>
2025-03-09 21:28:45 +0000

    Test apiview class

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,20 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# Test as an alternative to BookList
+class BookListView(APIView):
+    def get(self, request):
+        queryset = Book.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+        return Response(data=serializer.data)
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
