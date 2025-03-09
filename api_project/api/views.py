7afc23b969066b975dea396979f3047ca3379726
dev-dede <tsewayog@gmail.com>
2025-03-09 20:54:38 +0000

    Add permissions restriction to viewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,5 +9,13 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+    """
+    This view requires authentication.
+    Users must provide a valid token in the request header.
+    Example:
+        Authorization: Token <your_token_here>
+    """
+
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
