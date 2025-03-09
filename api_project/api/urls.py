3c51c935111ad4e8a59119783a9c12e28a40c13f
dev-dede <tsewayog@gmail.com>
2025-03-09 20:54:18 +0000

    Add route for generating tokens

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import BookList
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -12,4 +13,7 @@ urlpatterns = [
 
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),
+
+    # Include route for generating tokens
+    path('token-auth/', obtain_auth_token, name = 'api_token_auth')
 ]
