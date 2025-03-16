44f8d78bf3d3f0f5ca8c2d016b58fda3f234f884
dev-dede <tsewayog@gmail.com>
2025-03-16 22:44:43 +0000

    Remove extra space in import statement

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters  import rest_framework
+from django_filters import rest_framework
 
 class BookFilter(rest_framework.FilterSet):
     title = rest_framework.CharFilter(lookup_expr='icontains')
