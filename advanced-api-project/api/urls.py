ed73af50b0cfe65b67ea1fe11758128be754b2fc
dev-dede <tsewayog@gmail.com>
2025-03-16 15:40:20 +0000

    Add token authentication and isauthenticated permissions

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,6 +6,7 @@ from .views import (
     BookUpdateView,
     BookDeleteView,
 )
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name="Book-list"),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name="Book-create"),
     path('books/update/<int:pk>/', BookUpdateView.as_view(), name="Book-update"),
     path('books/delete/<int:pk>/', BookDeleteView.as_view(), name="Book-delete"),
+    path('token-auth/', views.obtain_auth_token)
 ]
