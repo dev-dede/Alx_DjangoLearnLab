e647225b0e00fba582469770cfada7a2bbff3e48
dev-dede <tsewayog@gmail.com>
2025-03-19 13:08:08 +0000

    Update url routes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path('accounts/profile/', Profile, name="profile"),
     path('home/', home, name="home"),
     path('posts/', PostListView.as_view(), name="post_list"),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
-    path('posts/new/', PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name="post_update"),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name="post_update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete"),
 ]
