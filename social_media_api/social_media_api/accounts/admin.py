fddb3acbfbfd32d31d359e49fec8993fed192396
dev-dede <tsewayog@gmail.com>
2025-03-29 23:19:13 +0000

    Set up customuser model

---
+++ social_media_api/social_media_api/accounts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "bio",
+        "profile_pic",
+    )
+
+    filter_horizontal = ("followers",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
